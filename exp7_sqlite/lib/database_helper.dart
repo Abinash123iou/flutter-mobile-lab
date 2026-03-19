5ed518a70aaca775bad0c6b72ec265a7ed99fa27
Abinash123iou <138517577+Abinash123iou@users.noreply.github.com>
2026-03-19 19:02:40 +0530

    Add database_helper.dart to exp7_sqlite

---
+++ exp7_sqlite/lib/database_helper.dart
@@ -0,0 +1,54 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  factory DatabaseHelper() => _instance;
+  DatabaseHelper._internal();
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, 'students.db');
+    return openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE students (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            marks INTEGER NOT NULL
+          )
+        ''');
+      },
+    );
+  }
+
+  Future<int> insertStudent(Map<String, dynamic> student) async {
+    final db = await database;
+    return await db.insert('students', student);
+  }
+
+  Future<List<Map<String, dynamic>>> getStudents() async {
+    final db = await database;
+    return await db.query('students', orderBy: 'id DESC');
+  }
+
+  Future<int> deleteStudent(int id) async {
+    final db = await database;
+    return await db.delete('students', where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<int> updateStudent(int id, Map<String, dynamic> student) async {
+    final db = await database;
+    return await db.update('students', student, where: 'id = ?', whereArgs: [id]);
+  }
+}
